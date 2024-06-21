a84bf9cfc47efc6af84be49ac1299bcc7f493c51
artuhreis <arthu.reis@hotmail.com>
2024-06-21 20:33:20 -0300

    Criado: Arquivo jogo_forca.py

---
+++ jogo_forca.py
@@ -0,0 +1,87 @@
+import random
+
+# Função para escolher uma palavra aleatória da lista
+def palavras():
+    lista_palavras = ["git ", "python", "linguagem", "programação", "HTML"]
+    return random.choice(lista_palavras)
+
+# Função para iniciar o jogo da forca
+def inicio_game():
+    palavra = palavras()  # Escolhe uma palavra aleatória
+    letras_acertadas = []  # Lista para armazenar as letras acertadas
+    tentativas = 7  # Número de tentativas que o jogador tem
+
+    # Inicializa a lista letras_acertadas para cada letra da palavra
+    for _ in palavra:
+        letras_acertadas.append('_')
+
+    print("Você ínicio o jogo da Forca!")
+    print("A palavra tem", len(palavra), "letras.") # Uma dica da quantidade de letras para o jogador
+    print(" ".join(letras_acertadas))  # Mostra o estado atual das letras acertadas
+
+    # Loop principal do jogo, continua enquanto o jogador tiver tentativas restantes
+    while tentativas > 0:
+        letra = input("Digite uma letra: ").lower()  # Recebe uma letra do jogador e converte para minúsculas
+
+        if letra in palavra:  # Verifica se a letra está na palavra
+            # Atualiza a lista letras_acertadas com a letra correta nas posições corretas
+            for i in range(len(palavra)):  # Itera sobre o comprimento da palavra
+                if palavra[i] == letra:  # Verifica se a letra no índice i da palavra é igual à letra digitada
+                    letras_acertadas[i] = letra  # Atualiza a lista letras_acertadas na posição i
+                    print("Você acertou!", letras_acertadas) 
+                    break
+
+            if '_' not in letras_acertadas:  # Verifica se todas as letras foram acertadas
+                print("Parabéns! Você acertou a palavra:", palavra)
+                break
+        else:
+            tentativas -= 1  # Diminui o número de tentativas se a letra não estiver na palavra
+            print("Você errou! Tentativas restantes:", tentativas)
+            if tentativas == 0:
+                print("Você perdeu! A palavra era:", palavra)
+                break
+
+# Função para exibir as regras do jogo e apresentar o sub-menu
+def regras():
+    print("Regras do Jogo da Forca:")
+    print("1. Uma palavra secreta será escolhida aleatoriamente.")
+    print("2. Você deve adivinhar a palavra letra por letra.")
+    print("3. Cada letra errada resulta na perda de uma tentativa.")
+    print("4. O jogo termina quando você adivinha a palavra ou perde todas as tentativas.")
+    print("5. Você tem 7 tentativas para acertar a Palavra.")
+    print("6. Boa sorte!\n")
+    print("1 - Jogar")
+    print("2 - Voltar ao Menu")
+    print("3 - Sair")
+
+    # Recebe a opção do jogador no sub-menu
+    opcao2 = int(input("Escolha uma opção: "))
+    if opcao2 == 1:
+        inicio_game()  # Inicia o jogo
+    elif opcao2 == 2:
+        menu()  # Volta ao menu principal
+    elif opcao2 == 3:
+        print("Obrigado volte sempre!")  # Sai do jogo
+    else:
+        print("Opção inválida. Tente novamente.")  # Trata uma entrada inválida
+
+# Função para exibir o menu principal do jogo
+def menu():
+    print("Bem vindo ao Jogo da Forca =)")
+    print("1 - Jogar")
+    print("2 - Regras")
+    print("3 - Sair")
+
+    # Recebe a opção do jogador no menu principal
+    opcao = int(input("Escolha uma opção: "))
+    if opcao == 1:
+        inicio_game()  # Inicia o jogo
+    elif opcao == 2:
+        regras()  # Exibe as regras e o sub-menu
+    elif opcao == 3:
+        print("Obrigado volte sempre!")  # Sai do jogo
+    else:
+        print("Opção inválida. Tente novamente.")  # Trata uma entrada inválida
+
+# Chama a função para exibir o menu principal
+menu()
